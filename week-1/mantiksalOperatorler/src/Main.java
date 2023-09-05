37973b2d4c4515b88370d934bcd01b9bbd3120fa
ahmetmuhammetturk <ahmetmuhammetturkbm@gmail.com>
2023-09-05 22:14:56 +0300

    mantiksal operatorler eklendi

---
+++ week-1/mantiksalOperatorler/src/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args) {
+
+      int a =10 , b=20, c;
+        c= (a==b) ? 2 : 3 ;
+        System.out.println(c);
+
+
+
+
+
+
+
+
+
+    }
+}
