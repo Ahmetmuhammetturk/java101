126dc0d8f8036434f9602fccced652b844eac566
ahmetmuhammetturk <ahmetmuhammetturkbm@gmail.com>
2023-09-05 23:58:01 +0300

    if eklendi

---
+++ week-1/if/src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+
+       int a=10 , b=20 , c= 1 ;
+
+        if ((a<b)&&(a<c))  {
+            System.out.println("A en küçük sayıdır.");
+
+        }else if ((b<a)&&(b<c)) {
+            System.out.println("B en küçük sayıdır.");
+        } else if ((c<a)&&(c<b)){
+            System.out.println("C en küçük sayıdır.");
+        }
+
+    }
+}
