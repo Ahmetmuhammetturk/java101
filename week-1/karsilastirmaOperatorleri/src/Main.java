7cae10b0211176c644f6ffac0e6df93922bb62cb
ahmetmuhammetturk <ahmetmuhammetturkbm@gmail.com>
2023-09-05 21:37:30 +0300

    hata

---
+++ week-1/karsilastirmaOperatorleri/src/Main.java
@@ -0,0 +1,44 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int A=10;
+        int B=20;
+        int C=10;
+        int D=40;
+
+        //== esit operatoru
+        System.out.println(A==B);
+        System.out.println(A==C);
+        System.out.println(A==D);
+
+        // != esit degil operatoru
+        System.out.println(A!=B);
+        System.out.println(A!=C);
+        System.out.println(A!=D);
+
+        // > büyüktür operatörü
+        System.out.println(A>B);
+        System.out.println(A>C);
+        System.out.println(A>D);
+
+
+        // >=büyük eşit operatörü
+
+        System.out.println(A>=B);
+        System.out.println(A>=C);
+        System.out.println(A>=D);
+
+
+        // < küçüktür operatörü
+        System.out.println(A<B);
+        System.out.println(A<C);
+        System.out.println(A<D);
+
+        // <= küçük eşit operatörü
+        System.out.println(A<=B);
+        System.out.println(A<=C);
+        System.out.println(A<=D);
+
+
+    }
+}
